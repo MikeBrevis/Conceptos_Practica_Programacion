cd84d6908664680eaf83fa343cbf53872ead9a27
MikeBrevis <michel.brevis@gmail.com>
2026-03-10 14:58:29 -0300

    python variables

---
+++ Python/1_variables.py
@@ -0,0 +1,18 @@
+print('mike')
+print("mike's laptop")
+print("mike " * 2)
+
+name = 'youtube'
+
+print(name[0]) # Print the first letter of the string
+print(name[-1]) # Print the last letter of the string
+print(name[0:2]) # Print the first two letters of the string
+print(name[2:4]) # Print the third and fourth letters of the string
+print(name[2:]) # Print the third letter to the end of the string
+print(name[:2]) # Print the first two letters of the string
+
+print('my ' + name[3:]) # Print the string 'my' + the third letter to the end of the string
+
+print(r'Telusko \n Rocks') # Print the string 'Telusko \n Rocks' as a raw string
+
+
