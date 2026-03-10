c80a112b25b7d1a9d405f9c43e7675166f8f6670
MikeBrevis <michel.brevis@gmail.com>
2026-03-10 17:42:33 -0300

    python variables ans strings

--- Python/1_variables.py
+++ Python/1_variables.py
@@ -10,9 +10,9 @@ print(name[0:2]) # Print the first two letters of the string
 print(name[2:4]) # Print the third and fourth letters of the string
 print(name[2:]) # Print the third letter to the end of the string
 print(name[:2]) # Print the first two letters of the string
+print('This print the string tube = ' + name[3:10])
 
 print('my ' + name[3:]) # Print the string 'my' + the third letter to the end of the string
-
 print(r'Telusko \n Rocks') # Print the string 'Telusko \n Rocks' as a raw string
 
 
