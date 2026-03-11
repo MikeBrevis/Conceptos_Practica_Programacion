b1829381387de7cb55cac00c23800418965c82d3
MikeBrevis <michel.brevis@gmail.com>
2026-03-11 11:10:59 -0300

    python basics, math operations

--- Python/1_basics.py
+++ Python/1_basics.py
@@ -1,8 +1,12 @@
+'''
+variables and strings
+'''
 print('mike')
 print("mike's laptop")
 print("mike " * 2)
 
-name = 'youtube'
+name = 'youtube' # variable with string
+number = 1 # variable with integer
 
 print(name[0]) # Print the first letter of the string
 print(name[-1]) # Print the last letter of the string
@@ -15,4 +19,17 @@ print('This print the string tube = ' + name[3:10])
 print('my ' + name[3:]) # Print the string 'my' + the third letter to the end of the string
 print(r'Telusko \n Rocks') # Print the string 'Telusko \n Rocks' as a raw string
 
+'''
+math operations
+'''
+print(5 + 3) # addition
+print(5 - 3) # subtraction
+print(5 * 3) # multiplication
+print(5 / 3) # division
+print(5 // 3) # intenger division
+print(5 % 3) # modulo
+print(5 ** 3) # exponentiation
+
+# example of math operations
+print(4 * 2 - 1 + (7 + 1) / (3 + 1)) # 9.0
 
