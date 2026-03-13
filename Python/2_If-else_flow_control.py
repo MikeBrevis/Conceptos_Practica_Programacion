31e7159c63b51015fa51148760fe558f1dcebff0
MikeBrevis <michel.brevis@gmail.com>
2026-03-13 16:51:42 -0300

    if-else flow control - bolean ands comparison operators

---
+++ Python/2_If-else_flow_control.py
@@ -0,0 +1,20 @@
+'''
+Bolean values
+'''
+spam = False
+spam2 = True
+
+print(spam)
+print(spam2)
+
+'''
+Comparison operators
+'''
+print(2 == 2) # True
+print(2 == 3) # False
+print(2 != 3) # True
+print(2 != 2) # False
+print(2 < 3) # True
+print(2 > 3) # False
+print(2 <= 3) # True
+print(2 >= 3) # False
